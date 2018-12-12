05295cf31712222c372ed95480db112d9ac812a6
rakhe93 <33520900+rakhe93@users.noreply.github.com>
2018-12-12 23:06:33 +0000

    Created Variables

--- src/carparkmanager/CarParkManager.java
+++ src/carparkmanager/CarParkManager.java
@@ -14,6 +14,10 @@ public class CarParkManager {
     /**
      * @param args the command line arguments
      */
+    
+    static int ticketNumber = 5000;
+    static String [][] parkSpace = new String [10][2];
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
