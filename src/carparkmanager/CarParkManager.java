183ae3b3afdb739cfa61c29cfd080d0c3e31aba6
rakhe93 <33520900+rakhe93@users.noreply.github.com>
2018-12-12 22:59:32 +0000

    Created skeleton

---
+++ src/carparkmanager/CarParkManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carparkmanager;
+
+/**
+ *
+ * @author Rakhe
+ */
+public class CarParkManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void Park(String str){
+        
+    }
+    
+    public static void Unpark(String str){
+        
+    }
+    
+    public static void Compact(String str){
+        
+    }
+    
+}
